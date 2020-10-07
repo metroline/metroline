2d0e2b3592db8cdebc583de244dc3cce7f3349f8
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-10-07 17:30:25 +0200

    fix: add checks for joining socket rooms

--- src/server/socket/rooms.ts
+++ src/server/socket/rooms.ts
@@ -2,24 +2,42 @@ import { Socket } from 'socket.io';
 import chalk from 'chalk';
 import { getUserFromSocket } from '../auth/auth';
 import { io } from './socket';
-import { pipelineSocketRoom } from '../pipelines/pipelines';
+import { Pipelines, pipelineSocketRoom } from '../pipelines/pipelines';
 import { repoSocketRoom } from '../repos/repo';
 import { jobSocketRoom } from '../jobs/job-socket';
 import { Logger } from '../../commons/logger/logger';
+import { canPullRepo } from '../repos/can-pull-repo';
+import { User } from '../users/user';
+import { ObjectId } from 'mongodb';
+import { Jobs } from '../jobs/jobs';
 
 const logger = new Logger('metroline.server:rooms');
 
-function joinRoom(socket: Socket, id: string, getRoom: (id: string) => string) {
+function joinRoom(
+  socket: Socket,
+  id: string,
+  getRoom: (id: string) => string,
+  checkAccess: (user: User) => Promise<boolean>,
+) {
   const room = getRoom(id);
   logger.debug(`${chalk.bold(socket.id)} asking to join room ${chalk.bold(room)}`);
+  let socketUser;
   getUserFromSocket(socket)
     .then(user => {
       if (!user) {
         logger.debug(`no user found in socket, not joining room ${chalk.bold(room)}`);
-        return;
+        return false;
       }
-      socket.join(room);
-      logger.debug(`${chalk.bold(user.name)} joined room ${chalk.bold(room)}`);
+      socketUser = user;
+      return checkAccess(user);
+    })
+    .then(canJoin => {
+      if (canJoin) {
+        socket.join(room);
+        logger.debug(`${chalk.bold(socketUser.name)} joined room ${chalk.bold(room)}`);
+      } else {
+        logger.debug(`${chalk.bold(socketUser.name)} is ${chalk.red('not')} allowed to join room ${chalk.bold(room)}`);
+      }
     })
     .catch(err => logger.error(err));
 }
@@ -28,14 +46,40 @@ io.on('connection', socket => {
   logger.debug(`Socket connected ${socket.id}`);
 
   socket.on('join.pipeline', pipelineId => {
-    joinRoom(socket, pipelineId, pipelineSocketRoom);
+    joinRoom(
+      socket,
+      pipelineId,
+      pipelineSocketRoom,
+      user => Pipelines()
+        .findOne({ _id: ObjectId.createFromHexString(pipelineId) })
+        .then(pipeline => (
+          pipeline ? canPullRepo(pipeline.repoId, user) : false
+        )),
+    );
   });
 
   socket.on('join.job', jobId => {
-    joinRoom(socket, jobId, jobSocketRoom);
+    joinRoom(
+      socket,
+      jobId,
+      jobSocketRoom,
+      user => Jobs()
+        .findOne({ _id: ObjectId.createFromHexString(jobId) })
+        .then(job => (
+          Pipelines().findOne({ _id: ObjectId.createFromHexString(job.pipelineId) })
+        ))
+        .then(pipeline => (
+          pipeline ? canPullRepo(pipeline.repoId, user) : false
+        )),
+    );
   });
 
   socket.on('join.repo', repoId => {
-    joinRoom(socket, repoId, repoSocketRoom);
+    joinRoom(
+      socket,
+      repoId,
+      repoSocketRoom,
+      user => canPullRepo(repoId, user),
+    );
   });
 });
