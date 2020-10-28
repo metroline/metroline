5f16274c4ef65bc069b35d8d7312643768356415
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-10-28 19:32:46 +0100

    refactor: enhance main error handler

--- src/commons/utils/handle-error.ts
+++ src/commons/utils/handle-error.ts
@@ -1,26 +1,34 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AxiosError } from 'axios';
 import { Logger } from '../logger/logger';
 import { HttpError } from '../errors/http-error';
+import { ValidationError } from 'joi';
 
 const logger = new Logger('metroline.server:handleError');
 
-export function handleError(err: any, req: Request, res: Response): void {
-  // TODO refactor how we handle error logging
+export function handleError(err: any, req: Request, res: Response, next: NextFunction): void {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  // TODO refactor
   if (!(err instanceof HttpError)) {
     logger.error(err.stack || err.message);
     if (err.isAxiosError) {
       logger.debug(JSON.stringify(err.toJSON(), null, 2));
       const axiosError: AxiosError = err as AxiosError;
-      logger.debug('response', JSON.stringify(axiosError.response, null, 2));
+      logger.debug('response', JSON.stringify(axiosError.response.data, null, 2));
     }
   }
+
+  const status = err instanceof ValidationError ? 400 : err?.statusCode || 500;
+  const error = err instanceof ValidationError ? err.details : err?.jsonResponse;
   res
-    .status(err.statusCode || 500)
+    .status(status)
     .send({
-      statusCode: err.statusCode || 500,
+      statusCode: status,
       path: req.path,
-      message: err.message || 'Internal error',
-      error: err.jsonResponse,
+      message: err?.message || 'Internal error',
+      error,
     });
 }
